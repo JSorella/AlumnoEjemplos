7dadc34b9743349d55f69925b2714f9a69496920
Jonathan Coralo <jonathan.corallo92@gmail.com>
2013-06-17 22:05:49 -0300

    Fix rotacion de camara

--- LosBorbotones/Pantallas/PantallaJuego.cs
+++ LosBorbotones/Pantallas/PantallaJuego.cs
@@ -282,7 +282,7 @@ escenario cargarse */
                 float rotAngulo = auto.mesh.Rotation.Y;
                 float aceleracionRotacion = 0.8f;
                 float deltaRotacion = rotAngulo - rotCamara;
-                if (deltaRotacion < 0) sentidoRotacion = -1;
+                if (deltaRotacion < 0 || ((3.15f < deltaRotacion) && (deltaRotacion < 6.2838f))) sentidoRotacion = -1;
                 else sentidoRotacion = 1;
                 if (rotAngulo != rotCamara)
                 {
